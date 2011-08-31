9d188bf9a7da893425ac3fa45cf9392acf65e7b1
Rein Henrichs <rein@phpfog.com>
2011-08-31 14:00:35 -0700

    A Turing Machine has a configuration list

--- lib/turing/machine.rb
+++ lib/turing/machine.rb
@@ -3,9 +3,10 @@ require 'turing/machine/configuration_list'
 
 module Turing
   class Machine
-    attr_reader :head
+    attr_reader :configuration_list, :head
 
     def initialize
+      @configuration_list = Turing::Machine::ConfigurationList.new
       @head = Turing::Machine::Head.new
     end
   end
--- spec/turing/machine_spec.rb
+++ spec/turing/machine_spec.rb
@@ -1,6 +1,10 @@
 require 'helper'
 
 describe Turing::Machine do
+  it "has a configuration list" do
+    Turing::Machine.new.configuration_list.must_be_instance_of Turing::Machine::ConfigurationList
+  end
+
   it "has a head" do
     Turing::Machine.new.head.must_be_instance_of Turing::Machine::Head
   end
