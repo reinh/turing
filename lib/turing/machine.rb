d585ef4beed091680992023563e198771e0d20be
Rein Henrichs <rein@phpfog.com>
2011-08-31 16:26:40 -0700

    Machine#current_state and #current_configuration
    * current_state is the current state label, starts at :b
    * current_configuration is the configuration for the current label and
      the symbol read from the tape at its current position

--- lib/turing/machine.rb
+++ lib/turing/machine.rb
@@ -3,11 +3,16 @@ require 'turing/machine/configuration_list'
 
 module Turing
   class Machine
-    attr_reader :configuration_list, :head
+    attr_reader :configuration_list, :current_state, :head
 
     def initialize
+      @current_state = :b # for begin
       @configuration_list = Turing::Machine::ConfigurationList.new
       @head = Turing::Machine::Head.new
+    end
+
+    def current_configuration
+      @configuration_list.get(@current_state, @head.read)
     end
   end
 end
--- spec/turing/machine_spec.rb
+++ spec/turing/machine_spec.rb
@@ -8,4 +8,18 @@ describe Turing::Machine do
   it "has a head" do
     Turing::Machine.new.head.must_be_instance_of Turing::Machine::Head
   end
+
+  describe "#current_state" do
+    it "defaults to :b" do
+      Turing::Machine.new.current_state.must_equal :b
+    end
+  end
+
+  describe "#current_configuration" do
+    it "gets the configuration for the current state and tape symbol" do
+      machine = Turing::Machine.new
+      config = machine.configuration_list.add :b, nil, [], :b
+      machine.current_configuration.must_equal config
+    end
+  end
 end
