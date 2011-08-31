38dc49516a68551d4f7b3bf3c918e9a2591baa1c
Rein Henrichs <rein@phpfog.com>
2011-08-31 12:05:26 -0700

    Add Turing::Tape#to_s

--- spec/turing_spec.rb
+++ spec/turing_spec.rb
@@ -40,5 +40,13 @@ describe Turing do
         Turing::Tape.new.inspect.must_include "<Turing::Tape>"
       end
     end
+
+    describe "#to_s" do
+      it "should join the characters" do
+        tape = Turing::Tape.new([1,2,3])
+        tape.to_s.must_equal tape.join
+        tape.to_s.must_equal "123"
+      end
+    end
   end
 end
