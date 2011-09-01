2d1e47ba4e254cadd17e153dd9a37441ee0f4842
Rein Henrichs <reinh@reinh.com>
2011-08-31 21:20:36 -0700

    Turing.from_io and .read_from_file helpers

--- lib/turing.rb
+++ lib/turing.rb
@@ -2,4 +2,12 @@ require 'turing/machine'
 require 'turing/parser'
 
 module Turing
+  def self.read_from_file(fh)
+    from_io(File.read(fh))
+  end
+
+  def self.from_io(io)
+    configurations = Turing::Parser.parse(io.read)
+    Turing::Machine.new(configurations)
+  end
 end
--- spec/turing_spec.rb
+++ spec/turing_spec.rb
@@ -1,4 +1,28 @@
 require 'helper'
+require 'stringio'
 
 describe Turing do
+  describe ".from_io" do
+    it "should parse the configurations from the file" do
+      machine_spec = <<-SPEC
+        b, None, P0R, c
+        c, None, R,   e
+        e, None, P1R, f
+        f, None, R,   b
+      SPEC
+
+      machine_spec_io = StringIO.new(machine_spec)
+
+      given = Turing.from_io(machine_spec_io)
+
+      expected = Turing::Machine.new([
+        [:b, nil, [[:write, 0], [:right]], :c],
+        [:c, nil, [[:right]],              :e],
+        [:e, nil, [[:write, 1], [:right]], :f],
+        [:f, nil, [[:right]],              :b]
+      ])
+
+      given.must_be :==, expected
+    end
+  end
 end
