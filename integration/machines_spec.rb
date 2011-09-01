e30214a37a203d2a88ef9ce7ec0a1c86af1f60fd
Rein Henrichs <reinh@reinh.com>
2011-08-31 18:53:40 -0700

    Stop using class var in test

--- integration/machines_spec.rb
+++ integration/machines_spec.rb
@@ -1,7 +1,21 @@
 require 'helper'
 
 describe "machines" do
-  describe "the .010101... machine" do
+  describe "the simple .010101... machine" do
+    describe "after 10 steps" do
+      it "should have a binary string output of \"01010101010101010101\"" do
+        machine = Turing::Machine.new
+        machine.configuration_list.add :b, nil, [[:write, 0], [:right]], :c
+        machine.configuration_list.add :c, nil, [[:right]], :e
+        machine.configuration_list.add :e, nil, [[:write, 1], [:right]], :f
+        machine.configuration_list.add :f, nil, [[:right]], :b
+        10.times { machine.step }
+        machine.head.tape.to_binary_s.must_equal "01010101010101010101"
+      end
+    end
+  end
+
+  describe "the condensed .010101... machine" do
     describe "after 10 steps" do
       it "should have a binary string output of \"01010101010101010101\"" do
         machine = Turing::Machine.new
--- spec/turing/machine/configuration_list_spec.rb
+++ spec/turing/machine/configuration_list_spec.rb
@@ -1,19 +1,18 @@
 require 'helper'
 
 describe Turing::Machine::ConfigurationList do
-  # Use ugly class var until minitest/spec let() works
-  before { @@list = Turing::Machine::ConfigurationList.new }
-
   describe "#add" do
     it "returns the configuration added" do
-      configuration = @@list.add :b, 0, [], :b
+      list = Turing::Machine::ConfigurationList.new
+      configuration = list.add :b, 0, [], :b
       configuration.must_be_instance_of Turing::Machine::Configuration
       configuration.to_a.must_equal [:b, 0, [], :b]
     end
 
     describe "when the action is unknown" do
       it "raises a Turing::Machine::Error" do
-        lambda { @@list.add :b, 0, [[:unknown]], :b }.must_raise Turing::Machine::Error
+        list = Turing::Machine::ConfigurationList.new
+        lambda { list.add :b, 0, [[:unknown]], :b }.must_raise Turing::Machine::Error
       end
     end
   end
@@ -21,15 +20,17 @@ describe Turing::Machine::ConfigurationList do
   describe "#get(label, symbol)" do
     describe "when there is no configuration with that label" do
       it "should return nil" do
-        configuration = @@list.add :b, 0, [], :b
-        @@list.get(:c, 0).must_be_nil
+        list = Turing::Machine::ConfigurationList.new
+        configuration = list.add :b, 0, [], :b
+        list.get(:c, 0).must_be_nil
       end
     end
 
     describe "when there is a configuration with that label that accepts that symbol" do
       it "should return the configuration" do
-        configuration = @@list.add :b, 0, [], :b
-        @@list.get(:b, 0).must_equal configuration
+        list = Turing::Machine::ConfigurationList.new
+        configuration = list.add :b, 0, [], :b
+        list.get(:b, 0).must_equal configuration
       end
     end
   end
